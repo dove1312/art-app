b01bf68a25a1160c5e5526849bc6b735c2dde939
dove1312 <dove1312@gmail.com>
2022-02-15 16:00:50 -0500

    display API data on page and add select element

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const artApp = {};
 artApp.apiKey = `fbC7vSXD`;
 artApp.apiUrl = `https://www.rijksmuseum.nl/api/en/collection`;
 
-artApp.getArt = function () {
+artApp.getArt = function (usersChosenAnimal) {
     const url = new URL(artApp.apiUrl);
     console.log(url);
 
     url.search = new URLSearchParams({
         key: artApp.apiKey,
-        q: 'monkey',
+        q: usersChosenAnimal,
         imgonly: true
     })
 
@@ -18,9 +18,50 @@ artApp.getArt = function () {
             return apiResponse.json();
         })
         .then(function (artFromTheApi) {
-            console.log(artFromTheApi.artObjects);
+            console.log(artFromTheApi.artObjects)
+            artApp.displayArt(artFromTheApi.artObjects)
         })
 
+}
+
+
+artApp.displayArt = function(artArray){
+    artArray.forEach(function(individualArtObject){
+        console.log(individualArtObject);
+        const artworkTitle = individualArtObject.title;
+        const artworkImage = individualArtObject.webImage.url;
+        const artist = individualArtObject.principalOrFirstMaker;
+        const altText = individualArtObject.longTitle;
+        
+
+        const listElement = document.createElement('li');
+        console.log(listElement)
+        listElement.classList.add('piece');
+
+        const heading = document.createElement('h2');
+        heading.textContent = artworkTitle;
+
+        const image = document.createElement('img');
+        image.src = artworkImage;
+        image.alt = altText;
+
+        const paragraphElement = document.createElement('p');
+        paragraphElement.classList.add('artist');
+        paragraphElement.textContent = artist;
+
+
+        listElement.appendChild(heading)
+        listElement.appendChild(image)
+        listElement.appendChild(paragraphElement)
+
+        //anotherway of writing this 
+        // listElement.append(heading, image, paragraphElement)
+
+        const ulElement = document.querySelector('#artwork');
+        ulElement.appendChild(listElement);
+
+
+    })
 }
 
 
@@ -31,11 +72,8 @@ artApp.getArt = function () {
 
 
 
-
-
-
 artApp.init = function () {
-    artApp.getArt();
+    artApp.getArt('whales');
 }
 
 artApp.init();
